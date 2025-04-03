ea964cdc9a142324db762ec8db7da94fd23b338b
Paulino <paulinomendes03@gmail.com>
2025-04-03 16:01:00 -0300

    feat: cria a base do projeto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { QuizComponent } from "../../components/quiz/quiz.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [QuizComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
